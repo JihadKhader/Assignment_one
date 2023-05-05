eb1a2a310e575824f6f17044e1bd8f69a422001e
JihadKhader <rashada1004@gmail.com>
2023-05-05 19:20:24 +0300

    Commit

--- app/src/main/java/firstlook/jihad/assignment_one/MainActivity.java
+++ app/src/main/java/firstlook/jihad/assignment_one/MainActivity.java
@@ -3,12 +3,23 @@ package firstlook.jihad.assignment_one;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+
+import java.util.ArrayList;
+import java.util.Arrays;
 
 public class MainActivity extends AppCompatActivity {
-
+    Spinner spinner;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        spinner = findViewById(R.id.spinner);
+
+        String[] courses = {"Chemistry","Math","Science","Test","Test","Test","English","Chemistry","Math","Science","Test","Test","Test","English","Arabic","History"};
+        ArrayList<String> arrayList = new ArrayList<>(Arrays.asList(courses));
+        ArrayAdapter<String> arrayAdapter = new ArrayAdapter<>(this,R.layout.spinner_style,arrayList);
+        spinner.setAdapter(arrayAdapter);
     }
 }
